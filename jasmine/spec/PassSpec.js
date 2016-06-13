ff21250f3850399b2e38d85447cd1180ab8b7596
bojangles495 <sirbojangles495@gmail.com>
2016-06-13 09:49:17 -0400

    Wrote tests for the GetPrepaidBalance and the UpdatePrepaidBalance methods

--- jasmine/spec/PassSpec.js
+++ jasmine/spec/PassSpec.js
@@ -152,4 +152,125 @@ describe("Pass",function(){
 		expect(createdPass.transportType).toEqual("bus");
 		expect(createdPass.passType).toEqual("monthly");
 	});
+
+	//Section will test the checkPrepaidPassBalance method
+	it("Pass GetPrepaidPassBalance should thrown an error because no input was put into the method", function(){
+		expect(function()
+		{
+			pass.getPrepaidPassBalance();
+		}).toThrowError("No input was entered");
+	});
+
+	it("Pass GetPrepaidPassBalance should throw an error because a non object was entered", function(){
+		expect(function()
+		{
+			pass.getPrepaidPassBalance("garabage");
+		}).toThrowError("A non boject was entered");
+
+		expect(function()
+		{
+			pass.getPrepaidPassBalance(45);
+		}).toThrowError("A non boject was entered");
+	});
+
+	it("Pass GetPrepaidPassBalance should throw an error because a null value was entered", function(){
+		expect(function()
+		{
+			pass.getPrepaidPassBalance(null);
+		}).toThrowError("A null value was entered");
+	});
+
+	it("Pass GetPrepaidPassBalance should throw an error because something other than a prepaid pass was entered", function(){
+		expect(function()
+		{
+			pass.getPrepaidPassBalance(pass.createPass(100,"bus","monthly"));
+		}).toThrowError("A non valid prepaid pass entered");
+	});
+
+	it("Pass GetPrepaidPassBalance should throw an error because a monthly pass was entered", function(){
+		expect(pass.getPrepaidPassBalance(pass.createPass(100,"bus","prepaid"))).toEqual(100);
+		expect(pass.getPrepaidPassBalance(pass.createPass(50.446,"subway","prepaid"))).toEqual(50.45);
+	});
+
+	//Section will test the updatePrepaidPassBalance method
+	it("Pass UpdatePrepaidPassBalance should throw an error because 1 or more input parameters were not entered", function(){
+		expect(function()
+		{
+			pass.updatePrepaidPassBalance();
+		}).toThrowError("1 or more input parameters are missing");
+
+		expect(function()
+		{
+			pass.updatePrepaidPassBalance(100);
+		}).toThrowError("1 or more input parameters are missing");
+	});
+
+	it("Pass UpdatePrepaidPassBalance should throw an error because 1 or more input parameters were null", function(){
+		expect(function()
+		{
+			pass.updatePrepaidPassBalance(null,100);
+		}).toThrowError("1 or more input parameters was null");
+
+		expect(function()
+		{
+			pass.updatePrepaidPassBalance(pass.createPass(100,"bus","prepaid"),null);
+		}).toThrowError("1 or more input parameters was null");
+	});
+
+	it("Pass UpdatePrepaidPassBalance should throw an error because the first input parameter must be a valid prepaid pass object", function(){
+		expect(function()
+		{
+			pass.updatePrepaidPassBalance("garabge",100);
+		}).toThrowError("The first input parameter must be a valid prepaid pass");
+
+		expect(function()
+		{
+			pass.updatePrepaidPassBalance(100,100);
+		}).toThrowError("The first input parameter must be a valid prepaid pass");
+
+		expect(function()
+		{
+			pass.updatePrepaidPassBalance(pass.createPass(100,"bus","monthly"),100);
+		}).toThrowError("The first input parameter must be a valid prepaid pass");
+	});
+
+	it("Pass UpdatePrepaidPassBalance should throw an error because the first input parameter must be a positive number greater than or equal to 0", function(){
+		newpass = pass.createPass(100,"bus","prepaid");
+		expect(function()
+		{
+			pass.updatePrepaidPassBalance(newpass,"garbage");
+		}).toThrowError("The second input parameter must be a number greater than or equal to 0");
+
+		expect(function()
+		{
+			pass.updatePrepaidPassBalance(newpass,"100");
+		}).toThrowError("The second input parameter must be a number greater than or equal to 0");
+
+		expect(function()
+		{
+			pass.updatePrepaidPassBalance(newpass,-3);
+		}).toThrowError("The second input parameter must be a number greater than or equal to 0");
+	});
+
+	it("Pass UpdatePrepaidPassBalance should throw an error because there were insufficient funds to subtract from the pass", function(){
+		expect(function()
+		{
+			pass.updatePrepaidPassBalance(pass.createPass(50,"bus","prepaid"),100);
+		}).toThrowError("There were insufficient funds");
+	});
+
+	it("Pass UpdatePrepaidPassBalance should return the newly updated pass", function(){
+		var newpass = pass.createPass(100,"bus","prepaid");
+
+		passCheck1 = pass.updatePrepaidPassBalance(newpass,100);//new balance should be 0
+		expect(passCheck1.balance).toEqual(0);
+
+		var newpass = pass.createPass(100,"bus","prepaid");
+		passCheck2 = pass.updatePrepaidPassBalance(newpass,0);//new balance should be 100
+		expect(passCheck2.balance).toEqual(100);
+
+		var newpass = pass.createPass(100,"bus","prepaid");
+		passCheck3 = pass.updatePrepaidPassBalance(newpass,50.764);//new balance should be 49.24 accounting for rounding down 50.764
+		expect(passCheck3.balance).toEqual(49.24);
+	});
 });
