5e2c595c002287b77d1ced78212e90a793775681
bojangles495 <sirbojangles495@gmail.com>
2016-06-12 20:01:04 -0400

    Added the methods CheckForPass, GetPass, and AssignPass methods

--- jasmine/src/User.js
+++ jasmine/src/User.js
@@ -86,4 +86,166 @@ User.prototype.getElderlyProperty = function()
 	}
 };
 
+User.prototype.checkForPass = function(passType,modeOfTransport)
+{
+	try
+	{
+		if(typeof passType !== "undefined" && typeof modeOfTransport !== "undefined")
+		{
+			if(passType === null || modeOfTransport === null)
+			{
+				throw new Error("1 or more of the parameters entered were null");
+			}
+			else if(typeof passType !== "string" || typeof modeOfTransport !== "string")
+			{
+				throw new Error("Both parameters entered need to be strings");
+			}
+			else
+			{
+				if(typeof this._listOfPasses !== "undefined")
+				{
+					if(typeof this._listOfPasses[modeOfTransport] !== "undefined")
+					{
+						if(typeof this._listOfPasses[modeOfTransport][passType] !== "undefined")
+						{
+							var pass = this._listOfPasses[modeOfTransport][passType];
+							if(passType === "prepaid")
+							{
+								if(typeof pass.balance === "number" &&
+									pass.transportType === modeOfTransport &&
+									pass.passType === passType)
+								{
+									return true;
+								}
+								else
+								{
+									throw new Error("Something other than an Instance of a Pass was added to the list of passes for the given passtype and mode of transportation");
+								}
+							}
+							else if(passType === "monthly")
+							{
+								if(typeof pass.paid === "number" &&
+									pass.transportType === modeOfTransport &&
+									pass.passType === passType)
+								{
+									return true;
+								}
+								else
+								{
+									throw new Error("Something other than an Instance of a Pass was added to the list of passes for the given passtype and mode of transportation");
+								}
+							}
+						}
+						else
+						{
+							return false;
+						}
+					}
+					else
+					{
+						return false;
+					}
+				}
+				else
+				{
+					return false;
+				}
+			}
+		}
+		else
+		{
+			throw new Error("1 or more parameters were not entered");
+		}
+	}
+	catch(err)
+	{
+		throw err;
+	}
+};
+
+User.prototype.getPass = function(passType,modeOfTransport)
+{
+	try
+	{
+		if(this.checkForPass(passType,modeOfTransport))
+		{
+			return this._listOfPasses[modeOfTransport][passType];
+		}
+		else
+		{
+			return false;
+		}
+	}
+	catch(err)
+	{
+		throw err;
+	}
+};
+
+User.prototype.assignPass = function(pass)
+{
+	try
+	{
+		if(typeof pass !== "undefined")
+		{
+			if(pass !== null)
+			{
+				if(typeof pass === "object" && 
+					typeof pass.passType !== "undefined" && 
+					typeof pass.transportType !== "undefined")
+				{
+					if(this.getPass(pass.passType,pass.transportType) === false)
+					{
+						if(pass.passType === "prepaid")
+						{
+							if(typeof pass.balance === "number")
+							{
+								this._listOfPasses = {};
+								this._listOfPasses[pass.transportType] = {};
+								this._listOfPasses[pass.transportType][pass.passType] = pass;
+							}
+							else
+							{
+								throw new Error("The object entered was not a valid object");
+							}
+						}
+						else if(pass.passType === "monthly")
+						{
+							if(typeof pass.paid === "number")
+							{
+								this._listOfPasses = {};
+								this._listOfPasses[pass.transportType] = {};
+								this._listOfPasses[pass.transportType][pass.passType] = pass;
+							}
+							else
+							{
+								throw new Error("The object entered was not a valid object");
+							}
+						}
+					}
+					else
+					{
+						throw new Error("The pass trying to be assigned already exists in the list of passes");
+					}
+				}
+				else
+				{
+					throw new Error("The object entered was not a valid object");
+				}
+			}
+			else
+			{
+				throw new Error("The value input was null");
+			}
+		}
+		else
+		{
+			throw new Error("A parameter was not input into the method");
+		}
+	}
+	catch(err)
+	{
+		throw err;
+	}
+}
 
