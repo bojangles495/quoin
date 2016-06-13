d508c589e0a6a6468bf1738b303882cad09a01e9
bojangles495 <sirbojangles495@gmail.com>
2016-06-13 09:52:17 -0400

    Wrote comments for the source code and created the GetPrePaidBalance and UpdatePrepaidBalance methods

--- jasmine/src/Pass.js
+++ jasmine/src/Pass.js
@@ -2,7 +2,10 @@
 function Pass(){
 };
 
-//takes in a fare and applies discounts to it and returns the new ammount
+/**
+Takes in a number corresponding to a fare or charge amount and decimal number corresponding to a discount 
+percentage applies discounts to the fare and returns the new ammount
+**/
 Pass.prototype.applyDiscounts = function(fare,discounts)
 {
 	try
@@ -45,6 +48,10 @@ Pass.prototype.applyDiscounts = function(fare,discounts)
 	}
 };
 
+/**
+The method takes in a number , string containing a transportation type, and a string that represents what the pass type will be
+and returns an object containing all of the pass information
+**/
 Pass.prototype.createPass = function(paid,transportType,passType)
 {
 	try
@@ -89,12 +96,109 @@ Pass.prototype.createPass = function(paid,transportType,passType)
 				
 				pass.transportType = transportType;
 				pass.passType = passType;
+				pass.expired = false;
 				return pass;
 			}
 		}
 		else
 		{
 			throw new Error("1 or more paramters are missing");
+		}
+	}
+	catch(err)
+	{
+		throw err;
+	}
+};
+
+/**
+The method takes in a prepaid pass object and will return the balance on that pass
+**/
+Pass.prototype.getPrepaidPassBalance = function(pass)
+{
+	try
+	{
+		if(typeof pass !== "undefined")
+		{
+			if(typeof pass === "object")
+			{
+				if(pass === null)
+				{
+					throw new Error("A null value was entered");
+				}
+				else
+				{
+					if(typeof pass.balance !== "undefined" && typeof pass.balance === "number")
+					{
+						return pass.balance;
+					}
+					else
+					{
+						throw new Error("A non valid prepaid pass entered");
+					}
+				}
+			}
+			else
+			{
+				throw new Error("A non boject was entered");
+			}
+		}
+		else
+		{
+			throw new Error("No input was entered");
+		}
+	}
+	catch(err)
+	{
+		throw err;
+	}
+};
+
+/**
+This method will take in a prepaid pass object and a number that corresponds to a fare that is 
+greater than or equal to 0 and return a new pass object that has the updated balance
+**/
+Pass.prototype.updatePrepaidPassBalance = function(pass,fare)
+{
+	try
+	{
+		if(typeof pass !== "undefined" && typeof fare !== "undefined")
+		{
+			if(pass !== null && fare !== null)
+			{
+				if(typeof pass === "object" && typeof pass.balance !== "undefined" && typeof pass.passType !== "undefined" && pass.passType === "prepaid")
+				{
+					if(typeof fare === "number" && fare >= 0)
+					{
+						var newBalance = pass.balance - Number(fare.toFixed(2));
+						if(newBalance >= 0)
+						{
+							pass.balance = newBalance;
+							return pass;
+						}
+						else
+						{
+							throw new Error("There were insufficient funds")
+						}
+					}
+					else
+					{
+						throw new Error("The second input parameter must be a number greater than or equal to 0");
+					}
+				}
+				else
+				{
+					throw new Error("The first input parameter must be a valid prepaid pass");
+				}
+			}
+			else
+			{
+				throw new Error("1 or more input parameters was null");
+			}
+		}
+		else
+		{
+			throw new Error("1 or more input parameters are missing");
 		}
 	}
 	catch(err)
